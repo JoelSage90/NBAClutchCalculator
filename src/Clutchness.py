0674cee59d9b6393d397784e7db5aa739720625f
unknown <joelsage90@gmail.com>
2025-07-20 18:21:45 +0100

    added page to compare 2 players. function complete. could do with some reformatting to make it look nicer

---
+++ src/Clutchness.py
@@ -0,0 +1,25 @@
+# streamlit_app.py
+# Page Title: Clutchness calculator
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from streamviz import gauge
+
+from collect_data import get_career_shot_data
+from shot_plot import draw_heat_map
+from clutch_score import clutchness_calculator
+from display_clutchness import display_clutchness
+
+st.set_page_config(layout="wide")
+
+
+st.title("NBA Clutchness Calculator")
+st.subheader("Select a player and see how clutch they are")
+player_list = pd.read_csv("../data/players.csv")
+player = st.selectbox("select a player",player_list["PLAYER"].unique(), index= None)
+
+#check for players who are already saved rather than making an api call
+cached_players = ["Kevin Durant","Stephen Curry", "LeBron James"]
+col1,col2 = st.columns([1,1.1])
+
+display_clutchness(player,col1,col2,player_list,cached_players)
---
+++ src/pages/Compare_Players.py
@@ -0,0 +1,27 @@
+# streamlit_app.py
+# Page Title: Compare Players
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+from display_clutchness import display_clutchness,compare_clutchness
+
+
+st.title("NBA Clutchness Comparer")
+st.subheader("Select 2 players and compare clutch they are")
+player_list = pd.read_csv("../data/players.csv")
+select_col1,select_col2 = st.columns(2)
+cached_players = ["Kevin Durant","Stephen Curry", "LeBron James"]
+with select_col1:
+    player1 = st.selectbox("Select Player 1",player_list["PLAYER"].unique(), index= None)
+
+with select_col2:
+    player2 = st.selectbox("Select Player 2",player_list["PLAYER"].unique(), index= None)
+
+if player1 is not None and player2 is not None:
+    col1, col2 = st.columns(2)
+    with col1:
+        compare_clutchness(player1, player_list, cached_players)
+    with col2:
+        compare_clutchness(player2, player_list, cached_players)   
